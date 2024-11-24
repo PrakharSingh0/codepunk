51702633674692056d4f85b0d901cf505c927ba9
PrakharSingh0 <prakhar2p@gmail.com>
2024-11-24 14:34:44 +0530

    project reset

--- lib/welcomePage.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-class welcomePage extends StatelessWidget {
-  const welcomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
